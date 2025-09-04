8b0a4dd8f1f2ffb1a1b38e23ea26d72ea9297863
tudeptrai <anhtunguyen643@gmail.com>
2025-09-05 01:09:43 +0700

    Closes #3: Completed About & Skills section cho portfolio

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,12 +2,13 @@ import TagManager from "react-gtm-module";
 import { ToastContainer } from "react-toastify";
 import Navbar from "./components/navbar";
 import HeroSection from "./components/hero-section";
+import AboutSection from "./components/about";
+import Skills from "./components/skills";
 import ScrollToTop from "./helpers/scroll-to-top";
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/600.css";
 import "@fontsource/inter/700.css";
-import"./card.scss"
-
+import "./card.scss";
 
 TagManager.initialize({ gtmId: "GTM-XXXXXXX" });
 
@@ -16,9 +17,10 @@ function App() {
     <>
       <ToastContainer />
       <div className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
-        <Navbar/>
+        <Navbar />
         <HeroSection />
-         
+        <AboutSection />
+    <Skills/>
         <ScrollToTop />
       </div>
     </>
