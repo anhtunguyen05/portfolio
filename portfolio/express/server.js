a070b2f429deb6d457c1c64dbd6edb73a6d35294
tudeptrai <anhtunguyen643@gmail.com>
2025-09-05 19:50:47 +0700

    Fix server.js for ESM (__dirname and path) #9

--- portfolio/express/server.js
+++ portfolio/express/server.js
@@ -3,6 +3,7 @@ import nodemailer from "nodemailer";
 import axios from "axios";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 import { fileURLToPath } from "url";
 
 dotenv.config();
